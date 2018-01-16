3f5cbb9a3201c625d9fb8dbc38087810a1502728
jasonleaster <jasonleaster@gmail.com>
2018-01-17 00:07:38 +0800

    Test util update

--- test/simpledb/systemtest/SystemTestUtil.java
+++ test/simpledb/systemtest/SystemTestUtil.java
@@ -1,5 +1,23 @@
 package simpledb.systemtest;
 
+import org.junit.Assert;
+import simpledb.BufferPool;
+import simpledb.Database;
+import simpledb.exception.DbException;
+import simpledb.Debug;
+import simpledb.HeapFileEncoder;
+import simpledb.transaction.TransactionId;
+import simpledb.Type;
+import simpledb.dbfile.DbFile;
+import simpledb.dbfile.HeapFile;
+import simpledb.exception.TransactionAbortedException;
+import simpledb.field.IntField;
+import simpledb.operator.OpIterator;
+import simpledb.operator.SeqScan;
+import simpledb.tuple.Tuple;
+import simpledb.tuple.TupleDesc;
+import simpledb.util.Utility;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,17 +26,15 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 
-import org.junit.Assert;
-
-import simpledb.*;
-
 public class SystemTestUtil {
     public static final TupleDesc SINGLE_INT_DESCRIPTOR =
             new TupleDesc(new Type[]{Type.INT_TYPE});
 
     private static final int MAX_RAND_VALUE = 1 << 16;
 
-    /** @param columnSpecification Mapping between column index and value. */
+    /**
+     * @param columnSpecification Mapping between column index and value.
+     */
     public static HeapFile createRandomHeapFile(
             int columns, int rows, Map<Integer, Integer> columnSpecification,
             ArrayList<ArrayList<Integer>> tuples)
@@ -26,7 +42,9 @@ public class SystemTestUtil {
         return createRandomHeapFile(columns, rows, MAX_RAND_VALUE, columnSpecification, tuples);
     }
 
-    /** @param columnSpecification Mapping between column index and value. */
+    /**
+     * @param columnSpecification Mapping between column index and value.
+     */
     public static HeapFile createRandomHeapFile(
             int columns, int rows, int maxValue, Map<Integer, Integer> columnSpecification,
             ArrayList<ArrayList<Integer>> tuples)
@@ -35,14 +53,14 @@ public class SystemTestUtil {
                 columnSpecification, tuples);
         return Utility.openHeapFile(columns, temp);
     }
-    
+
     public static HeapFile createRandomHeapFile(
             int columns, int rows, Map<Integer, Integer> columnSpecification,
             ArrayList<ArrayList<Integer>> tuples, String colPrefix)
             throws IOException, DbException, TransactionAbortedException {
         return createRandomHeapFile(columns, rows, MAX_RAND_VALUE, columnSpecification, tuples, colPrefix);
     }
-    
+
     public static HeapFile createRandomHeapFile(
             int columns, int rows, int maxValue, Map<Integer, Integer> columnSpecification,
             ArrayList<ArrayList<Integer>> tuples, String colPrefix)
@@ -53,8 +71,8 @@ public class SystemTestUtil {
     }
 
     public static File createRandomHeapFileUnopened(int columns, int rows,
-            int maxValue, Map<Integer, Integer> columnSpecification,
-            ArrayList<ArrayList<Integer>> tuples) throws IOException {
+                                                    int maxValue, Map<Integer, Integer> columnSpecification,
+                                                    ArrayList<ArrayList<Integer>> tuples) throws IOException {
         if (tuples != null) {
             tuples.clear();
         } else {
@@ -88,7 +106,7 @@ public class SystemTestUtil {
     public static ArrayList<Integer> tupleToList(Tuple tuple) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i = 0; i < tuple.getTupleDesc().numFields(); ++i) {
-            int value = ((IntField)tuple.getField(i)).getValue();
+            int value = ((IntField) tuple.getField(i)).getValue();
             list.add(value);
         }
         return list;
@@ -109,7 +127,7 @@ public class SystemTestUtil {
 
     public static void matchTuples(OpIterator iterator, List<ArrayList<Integer>> tuples)
             throws DbException, TransactionAbortedException, IOException {
-        ArrayList<ArrayList<Integer>> copy = new ArrayList<ArrayList<Integer>>(tuples);
+        ArrayList<ArrayList<Integer>> copy = new ArrayList<>(tuples);
 
         if (Debug.isEnabled()) {
             Debug.log("Expected tuples:");
@@ -148,6 +166,7 @@ public class SystemTestUtil {
 
     /**
      * Returns number of bytes of RAM used by JVM after calling System.gc many times.
+     *
      * @return amount of RAM (in bytes) used by JVM
      */
     public static long getMemoryFootprint() {
@@ -165,66 +184,71 @@ public class SystemTestUtil {
 
         return memAfter;
     }
-	
-	/**
-	 * Generates a unique string each time it is called.
-	 * @return a new unique UUID as a string, using java.util.UUID
-	 */
-	public static String getUUID() {
-		return UUID.randomUUID().toString();
-	}
-	
-	private static double[] getDiff(double[] sequence) {
-		double ret[] = new double[sequence.length - 1];
-		for (int i = 0; i < sequence.length - 1; ++i)
-			ret[i] = sequence[i + 1] - sequence[i];
-		return ret;
-	}
-	/**
-	 * Checks if the sequence represents a quadratic sequence (approximately)
-	 * ret[0] is true if the sequence is quadratic
-	 * ret[1] is the common difference of the sequence if ret[0] is true.
-	 * @param sequence
-	 * @return ret[0] = true if sequence is qudratic(or sub-quadratic or linear), ret[1] = the coefficient of n^2
-	 */
-	public static Object[] checkQuadratic(double[] sequence) {
-		Object ret[] = checkLinear(getDiff(sequence));
-		ret[1] = (Double)ret[1]/2.0;
-		return ret;
-	}
-	
-	/**
-	 * Checks if the sequence represents an arithmetic sequence (approximately)
-	 * ret[0] is true if the sequence is linear
-	 * ret[1] is the common difference of the sequence if ret[0] is true.
-	 * @param sequence
-	 * @return ret[0] = true if sequence is linear, ret[1] = the common difference
-	 */
-	public static Object[] checkLinear(double[] sequence) {				
-		return checkConstant(getDiff(sequence));
-	}
-	
-	/**
-	 * Checks if the sequence represents approximately a fixed sequence (c,c,c,c,..)
-	 * ret[0] is true if the sequence is linear
-	 * ret[1] is the constant of the sequence if ret[0] is true.
-	 * @param sequence
-	 * @return ret[0] = true if sequence is constant, ret[1] = the constant
-	 */
-	public static Object[] checkConstant(double[] sequence) {
-		Object[] ret = new Object[2];
-		//compute average
-		double sum = .0;
-		for(int i = 0; i < sequence.length; ++i)
-			sum += sequence[i];
-		double av = sum/(sequence.length + .0);
-		//compute standard deviation
-		double sqsum = 0;
-		for(int i = 0; i < sequence.length; ++i)
-			sqsum += (sequence[i] - av)*(sequence[i] - av);
-		double std = Math.sqrt(sqsum/(sequence.length + .0));
-		ret[0] = std < 1.0 ? Boolean.TRUE : Boolean.FALSE;
-		ret[1] = av;
-		return ret;
-	}
+
+    /**
+     * Generates a unique string each time it is called.
+     *
+     * @return a new unique UUID as a string, using java.util.UUID
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static double[] getDiff(double[] sequence) {
+        double ret[] = new double[sequence.length - 1];
+        for (int i = 0; i < sequence.length - 1; ++i)
+            ret[i] = sequence[i + 1] - sequence[i];
+        return ret;
+    }
+
+    /**
+     * Checks if the sequence represents a quadratic sequence (approximately)
+     * ret[0] is true if the sequence is quadratic
+     * ret[1] is the common difference of the sequence if ret[0] is true.
+     *
+     * @param sequence
+     * @return ret[0] = true if sequence is qudratic(or sub-quadratic or linear), ret[1] = the coefficient of n^2
+     */
+    public static Object[] checkQuadratic(double[] sequence) {
+        Object ret[] = checkLinear(getDiff(sequence));
+        ret[1] = (Double) ret[1] / 2.0;
+        return ret;
+    }
+
+    /**
+     * Checks if the sequence represents an arithmetic sequence (approximately)
+     * ret[0] is true if the sequence is linear
+     * ret[1] is the common difference of the sequence if ret[0] is true.
+     *
+     * @param sequence
+     * @return ret[0] = true if sequence is linear, ret[1] = the common difference
+     */
+    public static Object[] checkLinear(double[] sequence) {
+        return checkConstant(getDiff(sequence));
+    }
+
+    /**
+     * Checks if the sequence represents approximately a fixed sequence (c,c,c,c,..)
+     * ret[0] is true if the sequence is linear
+     * ret[1] is the constant of the sequence if ret[0] is true.
+     *
+     * @param sequence
+     * @return ret[0] = true if sequence is constant, ret[1] = the constant
+     */
+    public static Object[] checkConstant(double[] sequence) {
+        Object[] ret = new Object[2];
+        //compute average
+        double sum = .0;
+        for (int i = 0; i < sequence.length; ++i)
+            sum += sequence[i];
+        double av = sum / (sequence.length + .0);
+        //compute standard deviation
+        double sqsum = 0;
+        for (int i = 0; i < sequence.length; ++i)
+            sqsum += (sequence[i] - av) * (sequence[i] - av);
+        double std = Math.sqrt(sqsum / (sequence.length + .0));
+        ret[0] = std < 1.0 ? Boolean.TRUE : Boolean.FALSE;
+        ret[1] = av;
+        return ret;
+    }
 }
