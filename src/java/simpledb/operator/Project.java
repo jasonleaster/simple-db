0d4a4e1be7f86e43eab7104980a3565a45a88fd8
jasonleaster <jasonleaster@gmail.com>
2018-02-24 21:08:36 +0800

    refactor

--- src/java/simpledb/operator/Project.java
+++ src/java/simpledb/operator/Project.java
@@ -1,20 +1,19 @@
-package simpledb;
+package simpledb.operator;
 
+import simpledb.Type;
 import simpledb.exception.DbException;
 import simpledb.exception.TransactionAbortedException;
-import simpledb.operator.OpIterator;
-import simpledb.operator.Operator;
 import simpledb.tuple.Tuple;
 import simpledb.tuple.TupleDesc;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 /**
  * Project is an operator that implements a relational projection.
  */
 public class Project extends Operator {
 
-    private static final long serialVersionUID = 1L;
     private OpIterator child;
     private TupleDesc td;
     private ArrayList<Integer> outFieldIds;
@@ -22,47 +21,48 @@ public class Project extends Operator {
     /**
      * Constructor accepts a child operator to read tuples to apply projection
      * to and a list of fields in output tuple
-     * 
-     * @param fieldList
-     *            The ids of the fields child's tupleDesc to project out
-     * @param typesList
-     *            the types of the fields in the final projection
-     * @param child
-     *            The child operator
+     *
+     * @param fieldList The ids of the fields child's tupleDesc to project out
+     * @param typesList the types of the fields in the final projection
+     * @param child     The child operator
      */
     public Project(ArrayList<Integer> fieldList, ArrayList<Type> typesList,
-            OpIterator child) {
-        this(fieldList,typesList.toArray(new Type[]{}),child);
+                   OpIterator child) {
+        this(fieldList, typesList.toArray(new Type[]{}), child);
     }
-    
+
     public Project(ArrayList<Integer> fieldList, Type[] types,
-            OpIterator child) {
+                   OpIterator child) {
         this.child = child;
         outFieldIds = fieldList;
         String[] fieldAr = new String[fieldList.size()];
-        TupleDesc childtd = child.getTupleDesc();
+        TupleDesc childTupleDesc = child.getTupleDesc();
 
         for (int i = 0; i < fieldAr.length; i++) {
-            fieldAr[i] = childtd.getFieldName(fieldList.get(i));
+            fieldAr[i] = childTupleDesc.getFieldName(fieldList.get(i));
         }
         td = new TupleDesc(types, fieldAr);
     }
 
+    @Override
     public TupleDesc getTupleDesc() {
         return td;
     }
 
+    @Override
     public void open() throws DbException, NoSuchElementException,
             TransactionAbortedException {
-        child.open();
         super.open();
+        child.open();
     }
 
+    @Override
     public void close() {
         super.close();
         child.close();
     }
 
+    @Override
     public void rewind() throws DbException, TransactionAbortedException {
         child.rewind();
     }
@@ -70,9 +70,10 @@ public class Project extends Operator {
     /**
      * Operator.fetchNext implementation. Iterates over tuples from the child
      * operator, projecting out the fields from the tuple
-     * 
+     *
      * @return The next tuple, or null if there are no more tuples
      */
+    @Override
     protected Tuple fetchNext() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         while (child.hasNext()) {
@@ -89,15 +90,14 @@ public class Project extends Operator {
 
     @Override
     public OpIterator[] getChildren() {
-        return new OpIterator[] { this.child };
+        return new OpIterator[]{this.child};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
-	if (this.child!=children[0])
-	{
-	    this.child = children[0];
-	}
+        if (this.child != children[0]) {
+            this.child = children[0];
+        }
     }
-    
+
 }
