d887686a36ce77a9963e61aec629d3bda3dec9a8
jasonleaster <jasonleaster@gmail.com>
2018-03-05 23:04:38 +0800

    refactor

--- src/java/simpledb/IndexPredicate.java
+++ src/java/simpledb/IndexPredicate.java
@@ -9,44 +9,44 @@ import java.io.Serializable;
  * IndexPredicate compares a field which has index on it against a given value
  * @see IndexOpIterator
  */
-public class IndexPredicate implements Serializable {
-	
-    private static final long serialVersionUID = 1L;
+public class IndexPredicate {
 	
     private Predicate.Op op;
-    private Field fieldvalue;
+    private Field field;
 
     /**
      * Constructor.
      *
-     * @param fvalue The value that the predicate compares against.
+     * @param field The value that the predicate compares against.
      * @param op The operation to apply (as defined in Predicate.Op); either
-     *   Predicate.Op.GREATER_THAN, Predicate.Op.LESS_THAN, Predicate.Op.EQUAL,
-     *   Predicate.Op.GREATER_THAN_OR_EQ, or Predicate.Op.LESS_THAN_OR_EQ
+     *   Predicate.AggregateFunc.GREATER_THAN, Predicate.AggregateFunc.LESS_THAN, Predicate.AggregateFunc.EQUAL,
+     *   Predicate.AggregateFunc.GREATER_THAN_OR_EQ, or Predicate.AggregateFunc.LESS_THAN_OR_EQ
      * @see Predicate
      */
-    public IndexPredicate(Predicate.Op op, Field fvalue) {
+    public IndexPredicate(Predicate.Op op, Field field) {
         this.op = op;
-        this.fieldvalue = fvalue;
+        this.field = field;
     }
 
     public Field getField() {
-        return fieldvalue;
+        return field;
     }
 
     public Predicate.Op getOp() {
         return op;
     }
 
-    /** Return true if the fieldvalue in the supplied predicate
-        is satisfied by this predicate's fieldvalue and
-        operator.
-        @param ipd The field to compare against.
+    /**
+     * Return true if the field in the supplied predicate
+     * is satisfied by this predicate's field and operator.
+     *
+     * @param ipd The field to compare against.
     */
     public boolean equals(IndexPredicate ipd) {
-        if (ipd == null)
+        if (ipd == null) {
             return false;
-        return (op.equals(ipd.op) && fieldvalue.equals(ipd.fieldvalue));
+        }
+        return (op.equals(ipd.op) && field.equals(ipd.field));
     }
 
 }
