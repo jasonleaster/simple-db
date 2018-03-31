3a2dd5b225295063a7b3fce6aed75d2aadc17f8c
jasonleaster <jasonleaster@gmail.com>
2018-03-31 22:20:43 +0800

    refactor

--- src/java/simpledb/IndexOpIterator.java
+++ src/java/simpledb/IndexOpIterator.java
@@ -1,23 +1,28 @@
 package simpledb;
+
+import simpledb.exception.DbException;
 import simpledb.exception.TransactionAbortedException;
 import simpledb.operator.OpIterator;
 
-import java.util.*;
+import java.util.NoSuchElementException;
 
-/** IndexDBIterator is the interface that index access methods
-    implement in SimpleDb.
-*/
+/**
+ * IndexDBIterator is the interface that index access methods
+ * implement in SimpleDb.
+ * */
 public interface IndexOpIterator extends OpIterator {
-    /** Open the access method such that when getNext is called, it
-        iterates through the tuples that satisfy ipred.
-        @param ipred The predicate that is used to scan the index.
-    */
-    public void open(IndexPredicate ipred)
-        throws NoSuchElementException, DbException, TransactionAbortedException;
 
-    /** Begin a new index scan with the specified predicate.
-        @param ipred The predicate that is used to scan the index.
+    /**
+     * Open the access method such that when getNext is called, it
+     * iterates through the tuples that satisfy ipred.
+     *
+     * @param ipred The predicate that is used to scan the index.
     */
-    public void rewind(IndexPredicate ipred)
-        throws DbException, TransactionAbortedException;
+    void open(IndexPredicate ipred) throws NoSuchElementException, DbException, TransactionAbortedException;
+
+    /**
+     * Begin a new index scan with the specified predicate.
+     * @param ipred The predicate that is used to scan the index.
+    */
+    void rewind(IndexPredicate ipred) throws DbException, TransactionAbortedException;
 }
