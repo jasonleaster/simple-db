5464f222da6fbe0b43820a219ca3f441257b6643
jasonleaster <jasonleaster@gmail.com>
2018-04-06 22:13:15 +0800

    refactor

--- src/java/simpledb/CostCard.java
+++ src/java/simpledb/CostCard.java
@@ -1,14 +1,51 @@
 package simpledb;
+
+import simpledb.logical.LogicalJoinNode;
+
 import java.util.Vector;
 
-/** Class returned by {@link JoinOptimizer#computeCostAndCardOfSubplan} specifying the
-    cost and cardinality of the optimal plan represented by plan.
-*/
+/**
+ * Class returned by {@link JoinOptimizer#computeCostAndCardOfSubplan} specifying the
+ * cost and cardinality of the optimal plan represented by plan.
+ */
 public class CostCard {
-    /** The cost of the optimal subplan */
-    public double cost;
-    /** The cardinality of the optimal subplan */
-    public int card;
-    /** The optimal subplan */
-    public Vector<LogicalJoinNode> plan;
+
+    /**
+     * The cost of the optimal subplan
+     */
+    private double cost;
+
+    /**
+     * The cardinality of the optimal subplan
+     */
+    private int card;
+
+    /**
+     * The optimal subplan
+     */
+    private Vector<LogicalJoinNode> plan;
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getCard() {
+        return card;
+    }
+
+    public void setCard(int card) {
+        this.card = card;
+    }
+
+    public Vector<LogicalJoinNode> getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Vector<LogicalJoinNode> plan) {
+        this.plan = plan;
+    }
 }
